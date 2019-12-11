da6e4ab1717c514b7390754ab1ac4cbf23d384f0
Axis <atonymartin20@yahoo.com>
2019-12-11 08:34:30 -0600

    Homepage styling finished.

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -4,16 +4,18 @@ import withStyles from '@material-ui/core/styles/withStyles';
 
 const styles = theme => ({
     homepageDiv: {
-        width: '100%'
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
     },
     homepageSpacingDiv: {
-        marginTop: 65,
-        padding: '25px 4%',
-        [theme.breakpoints.up(710)]: {
-            marginLeft: 240
-        }
+        margin: '90px 3% 20px 3%',
+        width: '100%',
+        border: '2px solid #9cc7e3',
+        borderRadius: '2%',
+        backgroundColor: '#f8fbfd',
+        height: 500,
     },
-
 });
 
 class Homepage extends React.Component {
@@ -24,7 +26,6 @@ class Homepage extends React.Component {
             <div className={classes.homepageDiv}>
                 <Navbar />
                 <div className={classes.homepageSpacingDiv}>
-                    <h1>test</h1>
                 </div>
             </div>
         )
