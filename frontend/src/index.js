1abc113ebdba4f9b6a64fa1d7ffd822370fb344a
Axis <atonymartin20@yahoo.com>
2019-12-14 22:04:23 -0600

    Added another step to TDL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,3 +16,4 @@ ReactDOM.render(
 // 1. Add a add contact route
 // 2. Build add contact page
 // 3. Create on click for redirect on Edit button
+// 4. Create on Click for redirect on delete button
