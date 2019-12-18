f536df4a6cc5b2a3b9a3b22da37e8f3561e43ea9
Axis <atonymartin20@yahoo.com>
2019-12-17 22:32:56 -0600

    Added getContacts and addContact functions to context

---
+++ frontend/src/components/context/appContext.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export const AppContext = React.createContext();
+
+export default class AppProvider extends Component {
+    state = {
+        contacts: JSON.parse(localStorage.getItem('contacts')) || [],
+    };
+
+    render() {
+
+        return (
+            <AppContext.Provider
+                value={{
+                    state: this.state,
+                    getContacts: () => {
+                        const endpoint = '/contacts';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const contacts = res.data;
+                                localStorage.setItem('contacts', JSON.stringify(contacts));
+                                this.setState({
+                                    contacts
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting contacts', err)
+                            });
+                    },
+                    addContact: (firstName, lastName, phoneNumber, email, notes, cb) => {
+                        const endpoint = '/contacts';
+                        let contact = {
+                            firstName: firstName || null,
+                            lastName: lastName || null,
+                            phoneNumber: phoneNumber || null,
+                            email: email || null,
+                            notes: notes || null,
+                        };
+                        axios
+                            .post(endpoint, contact)
+                            .then(res => {
+                                contact = res.data;
+                                const joined = this.state.contacts.concat(contact);
+                                const index = joined.length - 1;
+                                localStorage.setItem('contacts', JSON.stringify(joined));
+                                this.setState({
+                                    contacts: joined
+                                });
+                                cb(index);
+                            })
+                            .catch(err => {
+                                console.log('error adding contact')
+                            })
+                    },
+
+                }}
+            >
+                {this.props.children}
+            </AppContext.Provider>
+        );
+    }
+}
