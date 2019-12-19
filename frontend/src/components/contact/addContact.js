bd3d2822f3f0565aebb9c89397b733ea0dc8741f
Axis <atonymartin20@yahoo.com>
2019-12-18 22:53:48 -0600

    addContact.js almost completed

--- frontend/src/components/contact/addContact.js
+++ frontend/src/components/contact/addContact.js
@@ -1,96 +1,20 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Navbar from '../navbar';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import { AppContext } from '../context/appContext.js';
 
 const styles = theme => ({
-    container: {
+    addContactDiv: {
         width: '100%',
-        padding: 0,
-        margin: 0,
         display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        fontSize: '1.75rem',
-        marginBottom: 3,
-        minHeight: 35,
-    },
-    name: {
-        width: '25%',
-        textAlign: 'center',
-        padding: '5px 10px',
-        [theme.breakpoints.down(1400)]: {
-            width: '32.5%'
-        },
-        [theme.breakpoints.down(1050)]: {
-            width: '40%',
-        },
-        [theme.breakpoints.down(950)]: {
-            width: '60%',
-        },
-        [theme.breakpoints.down(500)]: {
-            width: '100%',
-        },
-    },
-    phoneNumber: {
-        width: '20%',
-        textAlign: 'center',
-        padding: '5px 10px',
-        [theme.breakpoints.down(1400)]: {
-            width: '22.5%'
-        },
-        [theme.breakpoints.down(1052)]: {
-            width: '40%',
-        },
-        [theme.breakpoints.down(500)]: {
-            display: 'none',
-        },
-    },
-    email: {
-        width: '22.5%',
-        textAlign: 'center',
-        fontSize: '1.5rem',
-        overflowWrap: 'break-word',
-        padding: '5px 10px',
-        [theme.breakpoints.down(1400)]: {
-            width: '30%'
-        },
-        [theme.breakpoints.down(1052)]: {
-            display: 'none',
-        },
-    },
-    notes: {
-        width: '22.5%',
-        textAlign: 'center',
-        fontSize: '1.5rem',
-        padding: '5px 10px',
-        [theme.breakpoints.down(1400)]: {
-            display: 'none'
-        }
-    },
-    icons: {
-        width: '5%',
-        textAlign: 'center',
-        padding: '5px 10px',
-        [theme.breakpoints.down(1400)]: {
-            width: '7.5%'
-        },
-        [theme.breakpoints.down(1052)]: {
-            width: '10%',
-        },
-        [theme.breakpoints.down(950)]: {
-            display: 'none',
-        },
-    },
-    iconStyling: {
-        width: 25,
-        height: 25,
+        justifyContent: 'center',
     },
     addContactSpacingDiv: {
         margin: '90px 3% 20px 3%',
@@ -103,49 +27,152 @@ const styles = theme => ({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
-
-
     },
-    addContactDiv: {
+    card: {
         width: '100%',
+        maxWidth: '600px',
+        height: 'auto',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        backgroundColor: '#ffd8b1',
+    },
+    form: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+    inputHalfWidth: {
+        width: '45%',
+        [theme.breakpoints.down(900)]: {
+            width: '100%'
+        },
+        [theme.breakpoints.down(710)]: {
+            width: '45%'
+        },
+        [theme.breakpoints.down(500)]: {
+            width: '100%',
+        },
+    },
+    inputFullWidth: {
+        width: '100%',
+    },
+    inputLabelStyling: {
+        fontSize: '1.5rem'
+    },
+    inputStyling: {
+        fontSize: '1.5rem'
+    },
+    message: {
+        color: 'red',
+        fontSize: '1.5rem',
+    },
+    submitButton: {
+        width: '50%',
+        margin: '0 auto',
+        color: 'white',
+        backgroundColor: '#1565C0',
+        fontSize: '1.8rem',
+        textTransform: "none",
+        marginTop: 20,
+        "&:hover,&:focus": {
+            backgroundColor: "#AA1649",
+            color: "white"
+        },
     },
 });
 
 class AddContact extends React.Component {
     state = {
-        id: this.props.id,
-        firstName: this.props.firstName,
-        lastName: this.props.lastName,
-        phoneNumber: this.props.phoneNumber,
-        email: this.props.email,
-        notes: this.props.notes,
+        firstName: null,
+        lastName: null,
+        phoneNumber: null,
+        email: null,
+        notes: null,
+        message: '',
+        error: 0,
+        focus: 1,
     }
 
+    SubmitHandler = event => {
+        event.preventDefault();
+        const { firstName, lastName, phoneNumber, email, notes } = this.state;
+
+        if(!firstName && !lastName && !phoneNumber && !email && !notes) {
+            this.setState({
+                message: 'All fields cannot be empty',
+                focus: 1,
+                error: 1,
+            })
+        }
+
+        else if((firstName === null && lastName === null) || (firstName === '' || lastName === '')) {
+            this.setState({
+                firstName: 'Unknown',
+                lastName: 'Contact'
+            })
+        }
+
+    }
+
+    InputHandler = event => {
+        event.preventDefault();
+        const target = event.target;
+        this.setState({
+            [target.name]: target.value,
+            error: 0,
+            message: ''
+        })
+    }
     render() {
         const { classes } = this.props;
-        const { firstName, lastName, phoneNumber, email, notes } = this.state;
+
+        const labelStyle = {
+            fontSize: '1.5rem'
+        }
+        const textFieldStyle = {
+            fontSize: '1.5rem',
+        }
+
         return (
             <div className={classes.addContactDiv}>
                 <Navbar />
                 <div className={classes.addContactSpacingDiv}>
-                    <form className={classes.form} onSubmit={this.SubmitHandler}>
-                        <FormControl>
-
-                        </FormControl>
-                    </form>
-                    <Card className={classes.container}>
-                        <p className={classes.name}>firstName {this.props.lastName}</p>
-                        <p className={classes.phoneNumber}>1{phoneNumber}</p>
-                        <p className={classes.email}>2{email}</p>
-                        <p className={classes.notes}>3{notes}</p>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <p className={classes.message}>{this.state.message}</p>
+                            <form className={classes.form} onSubmit={this.SubmitHandler}>
+                                <FormControl className={classes.inputHalfWidth} error={this.state.error === 1 ? true: false}>
+                                    <InputLabel htmlFor='firstName' className={classes.inputLabelStyling}>First Name</InputLabel>
+                                    <Input autoFocus id="firstName" name="firstName" onChange={this.InputHandler} className={classes.inputStyling} />
+                                </FormControl>
+                                <FormControl className={classes.inputHalfWidth} error={this.state.error === 2 ? true: false}>
+                                    <InputLabel htmlFor='lastName' className={classes.inputLabelStyling}>Last Name</InputLabel>
+                                    <Input id="lastName" name="lastName" onChange={this.InputHandler} className={classes.inputStyling} />
+                                </FormControl>
+                                <FormControl className={classes.inputFullWidth} error={this.state.error === 3 ? true: false}>
+                                    <InputLabel htmlFor='phoneNumber' className={classes.inputLabelStyling}>Phone Number</InputLabel>
+                                    <Input id="phoneNumber" name="phoneNumber" onChange={this.InputHandler} className={classes.inputStyling} />
+                                </FormControl>
+                                <FormControl className={classes.inputFullWidth} error={this.state.error === 4 ? true: false}>
+                                    <InputLabel htmlFor='email' className={classes.inputLabelStyling}>Email</InputLabel>
+                                    <Input id="email" name="email" onChange={this.InputHandler} className={classes.inputStyling} />
+                                </FormControl>
+                                <FormControl className={classes.inputFullWidth} error={this.state.error === 5 ? true: false}>
+                                    <TextField multiline id="notes" name="notes" label="Notes" onChange={this.InputHandler} className={classes.inputStyling} InputLabelProps={{ style: labelStyle }} InputProps={{ style: textFieldStyle }} />
+                                </FormControl>
+                                <Button type='submit' variant='contained' className={classes.submitButton}>Submit</Button>
+                            </form>
+                        </CardContent>
                     </Card>
+
                 </div>
             </div>
         )
 
     }
 }
+
+AddContact.contextType = AppContext;
 
 export default withStyles(styles)(AddContact);
