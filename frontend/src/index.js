f765edf4eacdb051ec59e1172a3c2ed289e12cb2
Axis <atonymartin20@yahoo.com>
2019-12-21 22:55:09 -0600

    Removed To Do List from frontend and transitioning it to backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,12 +18,3 @@ ReactDOM.render(
     </AppProvider>,
     document.getElementById('root')
 );
-
-// TO DO LIST:
-// 10. Begin working on backend
-// 11. run yarn
-// 12. add cors, bcryptjs, express, helmet, knex, pg, sqlite3 as dependencies
-// 13. add nodemon as dev Dependency
-// 14. create knexfile.js
-// 15. add index.js
-// 16. Update heroku in this index.js and in README.md
