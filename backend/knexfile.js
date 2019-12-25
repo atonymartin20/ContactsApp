1653c7a97d5b0fce7fd9e071782c3214e7a0731e
Axis <atonymartin20@yahoo.com>
2019-12-25 14:35:38 -0600

    Still trying to get heroku to work

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
 
+require('dotenv').config('/.env');
+
 module.exports = {
 
   development: {
@@ -24,7 +26,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: './data/migrations'
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
     },
     seeds:{
       directory: './data/seeds'
